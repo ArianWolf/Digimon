71b5731bece02f7a58339f00360fa09798cc61e2
adinael <adinael_@hotmail.com>
2015-03-19 14:41:50 -0700

    Estilo de dropzone extrerno finalizado

--- app/scripts/apps/sources/dropzone/sourceDropzoneView.js
+++ app/scripts/apps/sources/dropzone/sourceDropzoneView.js
@@ -1,7 +1,6 @@
-import 'dropzone/dist/dropzone.css';
-import 'sweetalert/lib/sweet-alert.css';
+
 import Dropzone from 'dropzone';
-import SweetAlert from 'sweetalert/lib/sweet-alert';
+import 'dropzone/dist/dropzone.css';
 import App from 'app';
 import sourceDropzoneTemplate from 'apps/sources/dropzone/templates/sourceDropzone';
 
@@ -11,30 +10,64 @@ App.module('Sources.Dropzone.Views', function(Views, App, Backbone, Marionette) 
   class SourceDropzoneView extends Marionette.ItemView {
     constructor(...rest) {
       this.template = sourceDropzoneTemplate;
-      this.ui = { buttonNext: '.button-next'}
       super(...rest);
     }
 
     onShow() {
-      Dropzone.options.dropzone = {
-        clickable: '.clickable-dropzone',
-      };
+      this.createElements();
+    }
 
-      $(this.ui.buttonNext).on('click', function() {
-        SweetAlert("Bien!",
-        "Todo ha ocurrido con exito",
-        "success");
-
-        $('.confirm').on('click', function() {
-          App.router.navigate('/app/dashboard/crear/', { trigger: true });
-        });
+    createElements() {
+      // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+      var previewNode = document.querySelector("#template");
+      previewNode.id = "";
+      var previewTemplate = previewNode.parentNode.innerHTML;
+      previewNode.parentNode.removeChild(previewNode);
+       
+      var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+        url: "/target-url", // Set the url
+        thumbnailWidth: 80,
+        thumbnailHeight: 80,
+        parallelUploads: 20,
+        previewTemplate: previewTemplate,
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: "#previews", // Define the container to display the previews
+        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
       });
-
-      // activar cuando haya servidor
-      /*SweetAlert("Error!",
-        "Algo anda mal",
-        "error");*/
+       
+      myDropzone.on("addedfile", function(file) {
+        // Hookup the start button
+        file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+      });
+       
+      // Update the total progress bar
+      myDropzone.on("totaluploadprogress", function(progress) {
+        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+      });
+       
+      myDropzone.on("sending", function(file) {
+        // Show the total progress bar when upload starts
+        document.querySelector("#total-progress").style.opacity = "1";
+        // And disable the start button
+        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+      });
+       
+      // Hide the total progress bar when nothing's uploading anymore
+      myDropzone.on("queuecomplete", function(progress) {
+        document.querySelector("#total-progress").style.opacity = "0";
+      });
+       
+      // Setup the buttons for all transfers
+      // The "add files" button doesn't need to be setup because the config
+      // `clickable` has already been specified.
+      document.querySelector("#actions .start").onclick = function() {
+        myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+      };
+      document.querySelector("#actions .cancel").onclick = function() {
+        myDropzone.removeAllFiles(true);
+      };
     }
   }
+
   Views.SourceDropzoneView = SourceDropzoneView;
 });
