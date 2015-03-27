2ec1374e1e072ef8e68f718d553e59626e8e8dde
ArianWolf <pepezhe2@hotmail.com>
2015-03-27 11:40:26 -0700

    ready for rebase

---
+++ app/scripts/apps/dashboards/editor/dashboardEditorViewCopia.js
@@ -0,0 +1,61 @@
+import 'gridster.js/dist/jquery.gridster';
+import App from 'app';
+import EditorTemplate from 'apps/dashboards/editor/templates/editor';
+import PaneTemplate from 'apps/dashboards/editor/templates/pane';
+
+App.module('Dashboards.Editor.Views', function (Views, App, Backbone, Marionette) {
+  'use strict';
+
+  class PaneView extends Marionette.LayoutView {
+    constructor(...rest) {
+      this.tagName = 'li';
+      this.className = 'panel panel-default';
+      this.regions = { body: '.panel-body'};
+      super(...rest);
+    }
+
+    initialize() {
+      this.template = PaneTemplate;
+  
+      this.triggers = { 
+        'click .remove': 'remove:pane',
+        'click .options': 'options:pane'};
+    }
+  }
+
+  class DashboardEditorView extends Marionette.CompositeView {
+    constructor(...rest) {
+      this.className = 'gridster';
+      this.childView = PaneView;
+      this.childViewContainer = 'ul';
+      super(...rest);
+    }
+
+    initialize() {
+      this.template = EditorTemplate;
+      this.triggers = {
+        'click .add': 'add:pane',
+        'click .save': 'save:dashboard'
+      };
+    }
+
+    attachHtml(collectionView, childView) {
+      this.gridster.add_widget(childView.el, 1, 1); // jshint ignore:line
+    }
+
+    onBeforeRemoveChild(child) {
+      this.gridster.remove_widget(child.$el); // jshint ignore:line
+    }
+
+    onShow() {
+      this.gridster = $('.gridster ul').gridster({
+        widget_base_dimensions: [200, 200], // jshint ignore:line
+        resize: {
+          enabled: true
+        }
+      }).data('gridster');
+    }
+  }
+
+  Views.DashboardEditorView = DashboardEditorView;
+});
