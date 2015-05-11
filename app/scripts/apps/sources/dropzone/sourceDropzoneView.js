91eabb0b4f68d6e15fbe2ca5671bbc0546a3cb11
ArianWolf <pepezhe2@hotmail.com>
2015-05-11 12:00:26 -0600

    dropzone enhanced

--- app/scripts/apps/sources/dropzone/sourceDropzoneView.js
+++ app/scripts/apps/sources/dropzone/sourceDropzoneView.js
@@ -40,20 +40,13 @@ App.module('Sources.Dropzone.Views', function(Views, App, Backbone, Marionette) 
        
       // Update the total progress bar
       myDropzone.on('totaluploadprogress', function(progress) {
-        document.querySelector('#total-progress .progress-bar').style.width = progress + '%';
+        document.querySelector('.progress-bar').style.width = progress + '%';
       });
        
       myDropzone.on('sending', function(file) {
-      // Show the total progress bar when upload starts
-      document.querySelector('#total-progress').style.opacity = '1';
-      // And disable the start button
-      file.previewElement.querySelector('.start').setAttribute('disabled', 'disabled');
+        // And disable the start button
+        file.previewElement.querySelector('.start').setAttribute('disabled', 'disabled');
       });
-
-      // Hide the total progress bar when nothing's uploading anymore
-      myDropzone.on('queuecomplete', function() {
-      document.querySelector('#total-progress').style.opacity = '0';
-      }); 
            
       // Setup the buttons for all transfers
       // The 'add files' button doesn't need to be setup because the config
