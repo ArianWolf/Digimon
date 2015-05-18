4ca99d308c58b17d6b0a51bf89ea51358b1ac525
ArianWolf <pepezhe2@hotmail.com>
2015-05-17 18:02:44 -0600

    The items from dashboard list can be edited now

--- app/scripts/apps/dashboards/list/dashboardList.js
+++ app/scripts/apps/dashboards/list/dashboardList.js
@@ -7,27 +7,82 @@ App.module('Dashboards.List', function(List, App, Backbone, Marionette) {
   'use strict';
 
   class DashboardList extends Marionette.Object {
+    constructor(...rest) {
+      this.dashboardCollection = null;
+      super(...rest);
+    }
+
     showList(dashboards) {
+      this.dashboardCollection = dashboards;
       var region = this.getOption('region');
       var listView = new List.Views.DashboardListView({
         collection: dashboards 
       });
 
       this.listenTo(listView, 'childview:remove:item', (child) => {
-        this.sweetalertRemove(child);
+        this._sweetalertRemove(child);
+      });
+
+      this.listenTo(listView, 'childview:edit:item',(child) => {
+        var name = child.model.get('name');
+        var description = child.model.get('description');
+
+        var inputName = this._createInput(name, 'inputName');
+        this._cleanAndSetInput(child, 'name', inputName);
+
+        var inputDescription = this._createInput(description, 'inputDescription');
+        this._cleanAndSetInput(child, 'description', inputDescription);
+
+        this._setDisplayVauleToButtons(child, 'none', 'inline');
+      });
+
+      this.listenTo(listView, 'childview:save:item', (child) => {
+        var name = child.$('.inputName').val();
+        var description = child.$('.inputDescription').val();
+
+        child.$('.inputName').css('display', 'none');
+        child.$('.inputDescription').css('display', 'none');
+
+        child.$('#name').html(name);
+        child.$('#description').html(description);
+
+        this._setDisplayVauleToButtons(child, 'inline', 'none');
+
+        child.model.set('name', name);
+        child.model.set('description', description);
+
+        this.dashboardCollection.set(child.model ,{remove:false});
+
       });
       
       region.show(listView);
     } 
 
-    sweetalertRemove(child) {
+    _createInput(value, _class) {
+      return $('<input>').val(value).addClass(_class).
+        addClass('form-control');
+    }
+
+    _cleanAndSetInput(child, _id, input){
+      child.$('#' + _id).empty();
+      child.$('#' + _id).append(input);
+    }
+
+    _setDisplayVauleToButtons(child, edit, save) {
+      child.$('.editItem').css('display', edit);
+      child.$('.saveItem').css('display', save);
+    }
+
+
+
+    _sweetalertRemove(child) {
       Sweetalert({
-        title: '¿Esta seguro?',
-        text: 'Este registro se borrara permanentemente',
+        title: '¿Está seguro?',
+        text: 'Este registro se borrará permanentemente',
         type: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#DD6B55',
-        confirmButtonText: 'Si, Borrarlo',
+        confirmButtonText: 'Sí, Borrarlo',
         cancelButtonText: 'No, Cancelar',
         closeOnConfirm: false,
         closeOnCancel: false
@@ -37,7 +92,7 @@ App.module('Dashboards.List', function(List, App, Backbone, Marionette) {
             Sweetalert('¡Borrado!', 'El registro ha sido borrado.', 'success');
             child.model.destroy();
           } else {
-            Sweetalert('¡Cancelado!', 'Su archivo esta a salvo :)', 'error');
+            Sweetalert('¡Cancelado!', 'Su registro está a salvo :)', 'error');
         }
       });
     }
