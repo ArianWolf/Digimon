b32b423437496280b67fd4832f3807a089051c02
ArianWolf <pepezhe2@hotmail.com>
2015-06-01 20:16:44 -0600

    gropus secction added

--- app/scripts/apps/users/profile/userProfile.js
+++ app/scripts/apps/users/profile/userProfile.js
@@ -21,9 +21,79 @@ App.module('Users.Profile', function(Profile, App, Backbone, Marionette) {
       regionList.show(userListView);
 
       this.listenTo(profileView, 'edit:info', () => {
-        this._changeDisableStatus(profileView, false);
+        var currentName = profileView.$('#name').html();
+        var currentUsername = profileView.$('#username').html();
+        var currentEmail= profileView.$('#email').html();
 
-        profileView.$('#firstname').focus();
+        var nameEdit = profileView.$('#name-edit').val(currentName);
+        var usernameEdit = profileView.$('#username-edit').val(currentUsername);
+        var emailEdit = profileView.$('#email-edit').val(currentEmail);
+
+
+        profileView.$('#modal-edit-info').modal('show');
+      });
+
+      this.listenTo(profileView, 'edit:pass', () => {
+        profileView.$('#modal-edit-pass').modal('show');
+      });
+
+      this.listenTo(profileView, 'complete:save', () => {
+
+        var nameEdit = profileView.$('#name-edit').val();
+        var usernameEdit = profileView.$('#username-edit').val();
+        var emailEdit = profileView.$('#email-edit').val();
+
+
+
+        if(nameEdit === '' || usernameEdit === '' || emailEdit === '') {
+          Sweetalert("Error", "Complete todos los espacios para continuar", "error");
+        } else {
+
+          if(nameEdit.length > 27 || usernameEdit.length > 27 || emailEdit.length > 27) {
+            Sweetalert("Error", "Los campos no deben superar los 27 caracteres", "error");
+          } else { 
+            profileView.$('#name').empty('');
+            profileView.$('#name2').empty('');
+            profileView.$('#username').empty('');
+            profileView.$('#email').empty('');
+
+            profileView.$('#name').html(nameEdit);
+            profileView.$('#name2').html(nameEdit);
+            profileView.$('#username').html(usernameEdit);
+            profileView.$('#email').html(emailEdit);
+
+            profileView.$('#modal-edit-info').modal('hide');
+
+            Sweetalert("Éxito", "Su informacion a sido actualizada", "success");
+          }
+        }
+      });
+
+      this.listenTo(profileView, 'complete:pass', () => {
+        var currentPass = profileView.$('#current-pass').val();
+        var newPass = profileView.$('#new-pass').val();
+        var reNewPass = profileView.$('#re-new-pass').val();
+
+        if(currentPass === '' || newPass !== reNewPass || newPass === '' || reNewPass === '') {
+          Sweetalert("Error", "Complete todos los campos y verifique que las nuevas contraseñas conicidan", "error");
+        } else {
+          Sweetalert("Éxito", "Su contraseña ha sido cambiada", "success");
+          profileView.$('#modal-edit-pass').modal('hide');
+          profileView.$('#current-pass').val('');
+          profileView.$('#new-pass').val('');
+          profileView.$('#re-new-pass').val('');
+        } 
+      });
+
+      this.listenTo(profileView, 'create:group', () => {
+        var groupName = profileView.$('#group-name').val();
+        if(groupName !== '') {
+          Sweetalert("Éxito", "Su grupo ha sido creado", "success");
+          profileView.$('#group-name').val('');
+        } else {
+          Sweetalert("Error", "Introduzaca un nombre para el grupo", "error");
+        }
+        
       });
 
       this.listenTo(profileView, 'save:info', () => {
@@ -37,16 +107,6 @@ App.module('Users.Profile', function(Profile, App, Backbone, Marionette) {
         var icono = $('<i>').addClass('fa fa-check');
         child.$('.add').append(icono);
       });
-    }
-
-    _changeDisableStatus(view , value){
-      view.$('#firstname').prop('disabled', value);
-      view.$('#lastname').prop('disabled', value);
-      view.$('#username').prop('disabled', value);
-      view.$('#password').prop('disabled', value);
-      view.$('#confirm').prop('disabled', value);
-      view.$('#email').prop('disabled', value);
-      view.$('#save').prop('disabled', value);
     }
   }
 
